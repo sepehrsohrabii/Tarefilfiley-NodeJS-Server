fd51a90b2fe968c2cf3b592c926c1a6eeaa0aedc
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2023-04-24 23:42:32 +0330

    UPDATE

--- helpers/session.js
+++ helpers/session.js
@@ -10,7 +10,7 @@ const sessionMiddleware = Session({
   saveUninitialized: false,
   cookie: { secure: false },
   store: MongoStore.create({
-    mongoUrl: process.env.MONGODB_URL,
+    mongoUrl: process.env.MONGODB_URI,
     ttl: 1 * 24 * 60 * 60, // session expires in 1 day
   }),
 });
